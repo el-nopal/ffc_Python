edcca98922ef695538bceff3d4c55fd9c57d2803
mike v <xvx1138@protonmail.com>
2019-08-28 10:17:53 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+print("hello world")
+
+# example 2
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+# example 3
+character_name = "john"
+character_age = "35"
+print("there once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+print("he really liked the name " + character_name + ",")
+print("but didn't like being " + character_age + ".")
