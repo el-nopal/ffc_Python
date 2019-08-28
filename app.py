d1724ff558ad5f8e42c4234645cb471b47fcdeb1
mike v <xvx1138@protonmail.com>
2019-08-28 14:36:54 +0200

    mad libs example

--- app.py
+++ app.py
@@ -13,3 +13,20 @@ print("there once was a man named " + character_name + ",")
 print("he was " + character_age + " years old.")
 print("he really liked the name " + character_name + ",")
 print("but didn't like being " + character_age + ".")
+
+# example 4
+print("string example")
+
+# basic calculator
+num1 = input("enter a number: ")
+num2 = input("enter another number: ")
+result = int(num1) + int(num2)
+print(result)
+
+# mad libs
+color = input("enter color: ")
+plural_noun = input("enter plural noun: ")
+celebrity = input("enter celebrity name: ")
+print("roses are " + color)
+print(plural_noun + " are blue")
+print("i love " + celebrity)
